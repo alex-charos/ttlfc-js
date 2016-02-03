8f421080057796e25d4050dcc8c32502d0ee9bfa
Alex <realtowz@yahoo.com>
2016-02-03 22:20:33 +0200

    hardcoded ttlfc url

--- app/scripts/services/top-trumps-services.js
+++ app/scripts/services/top-trumps-services.js
@@ -1,12 +1,12 @@
 'use strict';
 var topTrumpService = angular.module('topTrumpService', ['ngResource']);
 topTrumpService.factory('ttService',[ '$resource', function($resource) {
-   return $resource('/tt', {}, {
-   		enterLobby: {method:'POST', url:'/tt/enter-lobby'},
-   		viewHand: {method: 'GET', url:'/tt/game/:gameId/view-hand/:playerId'},
-   		executeTrick: {method: 'POST', url:'/tt/game/:gameId/trick/attribute/:attribute'},
-   		checkStatus: {method: 'GET', url:'/tt/my-update/:playerId'},
-   		getPlayerTurn: {method:'GET', url:'/tt/game/turn/:gameId'}
+   return $resource('https://ttlfc.herokuapp.com/tt', {}, {
+   		enterLobby: {method:'POST', url:'https://ttlfc.herokuapp.com/tt/enter-lobby'},
+   		viewHand: {method: 'GET', url:'https://ttlfc.herokuapp.com/tt/game/:gameId/view-hand/:playerId'},
+   		executeTrick: {method: 'POST', url:'https://ttlfc.herokuapp.com/tt/game/:gameId/trick/attribute/:attribute'},
+   		checkStatus: {method: 'GET', url:'https://ttlfc.herokuapp.com/tt/my-update/:playerId'},
+   		getPlayerTurn: {method:'GET', url:'https://ttlfc.herokuapp.com/tt/game/turn/:gameId'}
       
     });
 }]);
